1aec03e0c88c311bbb02f15a88bb7ea291d59928
Beroni <beronimagalhaes@gmail.com>
2020-09-12 14:12:22 -0300

    Update user Model

--- src/models/user.go
+++ src/models/user.go
@@ -2,16 +2,18 @@ package model
 
 import (
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 //User
 type User struct {
-	ID        string    `json:"_id" bson:"_id"`
-	Email     string    `json:"email" bson:"email"`
-	Name      string    `json:"name" bson:"name"`
-	Password  string    `json:"password" bson:"password"`
-	CreatedAt time.Time `json:"created_at" bson:"created_at"`
-	UpdatedAt time.Time `json:"created_at" bson:"updated_at"`
+	ID        primitive.ObjectID `json:"_id" bson:"_id"`
+	Email     string             `bson:"email"`
+	Name      string             `bson:"name"`
+	Password  string             `bson:"password"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at"`
 }
 
 //Users
