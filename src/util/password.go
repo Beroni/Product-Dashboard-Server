ce87fccc775aa8c815bc522d8dfb03f66806fb44
Beroni <beronimagalhaes@gmail.com>
2020-09-12 09:41:33 -0300

    Add password encryption

---
+++ src/util/password.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(pwd string) string {
+
+	password := []byte(pwd)
+
+	hash, err := bcrypt.GenerateFromPassword(password, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return string(hash)
+}
+
+func CheckPasswordHash(hashedPwd string, password string) bool {
+
+	plainPwd := []byte(password)
+
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, plainPwd)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	return true
+}
