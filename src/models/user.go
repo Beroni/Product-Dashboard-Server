47dd2a794c4cd664875ae95e071c6034eaeb37db
Beroni <beronimagalhaes@gmail.com>
2020-09-12 22:17:38 -0300

    Refatoring User model and Add Validators

--- src/models/user.go
+++ src/models/user.go
@@ -3,18 +3,39 @@ package model
 import (
 	"time"
 
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 //User
 type User struct {
 	ID        primitive.ObjectID `json:"_id" bson:"_id"`
-	Email     string             `bson:"email"`
-	Name      string             `bson:"name"`
-	Password  string             `bson:"password"`
-	CreatedAt time.Time          `bson:"created_at"`
-	UpdatedAt time.Time          `bson:"updated_at"`
+	Email     string             `json:"email" bson:"email"`
+	Name      string             `json:"name" bson:"name"`
+	Password  string             `json:"password" bson:"password"`
+	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
 }
 
 //Users
 type Users []User
+
+func (user User) ValidateSignUp() error {
+
+	return validation.ValidateStruct(&user,
+		validation.Field(&user.Email, validation.Required, is.Email),
+		validation.Field(&user.Name, validation.Required),
+		validation.Field(&user.Password, validation.Required, validation.Length(5, 0)),
+	)
+
+}
+
+func (user User) ValidateSignIn() error {
+
+	return validation.ValidateStruct(&user,
+		validation.Field(&user.Email, validation.Required, is.Email),
+		validation.Field(&user.Password, validation.Required, validation.Length(5, 0)),
+	)
+
+}
