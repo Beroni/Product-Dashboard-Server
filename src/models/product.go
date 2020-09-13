edc0f0d9f0c1eb4967ec8ee1beda0338dfff6465
Beroni <beronimagalhaes@gmail.com>
2020-09-12 22:17:50 -0300

    Refatoring Product model and Add Validators

--- src/models/product.go
+++ src/models/product.go
@@ -3,10 +3,13 @@ package model
 import (
 	"time"
 
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-//Product
+//Product -> Product Model
 type Product struct {
 	ID        primitive.ObjectID `json:"_id" bson:"_id"`
 	Name      string             `json:"name" bson:"name"`
@@ -16,5 +19,27 @@ type Product struct {
 	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
 }
 
-//Products
+//Products -> Array of Products
 type Products []Product
+
+//ValidateCreation -> Validator to the post request.
+func (product Product) ValidateCreation() error {
+
+	return validation.ValidateStruct(&product,
+		validation.Field(&product.Name, validation.Required),
+		validation.Field(&product.Price, validation.Required, is.Float),
+		validation.Field(&product.Quantity, validation.Required, is.Int),
+	)
+
+}
+
+//ValidateUpdate -> Validator to the put request.
+func (product Product) ValidateUpdate() error {
+
+	return validation.ValidateStruct(&product,
+		validation.Field(&product.Name),
+		validation.Field(&product.Price, is.Float),
+		validation.Field(&product.Quantity, is.Int),
+	)
+
+}
